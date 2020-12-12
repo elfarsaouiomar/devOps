88edfd1fd80ed3167bc4582ebcdc8e1cfedaf12a
omar elfarsaoui <adilfarsaoui7@gmail.com>
2020-12-12 02:27:31 +0100

    * test edit online

--- laravel-app/resources/views/products/index.blade.php
+++ laravel-app/resources/views/products/index.blade.php
@@ -40,7 +40,7 @@
                     @csrf
                     @method('DELETE')
       
-                    <button type="submit" class="btn btn-danger">Delete</button>
+                    <button type="submit" class="btn btn-danger">Delete me</button>
                 </form>
             </td>
         </tr>
