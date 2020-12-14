0f65007a988fd9c37faf88bd35126cf473e0ff4d
omar elfarsaoui <adilfarsaoui7@gmail.com>
2020-12-14 14:07:39 +0100

    * test rollback

--- laravel-app/resources/views/products/index.blade.php
+++ laravel-app/resources/views/products/index.blade.php
@@ -40,7 +40,7 @@
                     @csrf
                     @method('DELETE')
       
-                    <button type="submit" class="btn btn-danger">Delete</button>
+                    <button type="submit" class="btn btn-danger">delete this product</button>
                 </form>
             </td>
         </tr>
