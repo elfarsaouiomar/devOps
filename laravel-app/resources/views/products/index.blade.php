9e4e7bd426ef83e239770455100c7c78eac0e89a
omar elfarsaoui <adilfarsaoui7@gmail.com>
2020-12-14 16:10:46 +0100

    fix error text

--- laravel-app/resources/views/products/index.blade.php
+++ laravel-app/resources/views/products/index.blade.php
@@ -40,7 +40,7 @@
                     @csrf
                     @method('DELETE')
       
-                    <button type="submit" class="btn btn-danger">Delete ythis product</button>
+                    <button type="submit" class="btn btn-danger">Delete</button>
                 </form>
             </td>
         </tr>
