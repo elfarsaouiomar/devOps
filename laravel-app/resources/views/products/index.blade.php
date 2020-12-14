c33deaebde14dc6ad104c05e48bd4bdb185ed18d
omar elfarsaoui <adilfarsaoui7@gmail.com>
2020-12-14 01:18:20 +0100

    test CI/CD again

--- laravel-app/resources/views/products/index.blade.php
+++ laravel-app/resources/views/products/index.blade.php
@@ -40,7 +40,7 @@
                     @csrf
                     @method('DELETE')
       
-                    <button type="submit" class="btn btn-danger">Delete This One</button>
+                    <button type="submit" class="btn btn-danger">Delete</button>
                 </form>
             </td>
         </tr>
